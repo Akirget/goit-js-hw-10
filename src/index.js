70924fe7a58b5be9a65775584a0990b14d16950b
Kirhet Oleksandr <57485036+Akirget@users.noreply.github.com>
2022-10-03 13:48:26 +0200

    Домашнє завдання JS №10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,73 @@
 import './css/styles.css';
+import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputElement = document.getElementById('search-box');
+const listElement = document.querySelector('.country-list');
+const infoElement = document.querySelector('.country-info');
+
+const cleanMarkup = ref => (ref.innerHTML = '');
+
+const inputHandler = e => {
+  const textInput = e.target.value.trim();
+
+  if (!textInput) {
+    cleanMarkup(listElement);
+    cleanMarkup(infoElement);
+    return;
+  }
+
+  fetchCountries(textInput)
+    .then(data => {
+      console.log(data);
+      if (data.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name'
+        );
+        return;
+      }
+      renderMarkup(data);
+    })
+    .catch(err => {
+      cleanMarkup(listElement);
+      cleanMarkup(infoElement);
+      Notify.failure('Oops, there is no country with that name');
+    });
+};
+inputElement.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
+
+const renderMarkup = data => {
+  if (data.length === 1) {
+    cleanMarkup(listElement);
+    const markupInfo = createInfoMarkup(data);
+    infoElement.innerHTML = markupInfo;
+  } else {
+    cleanMarkup(infoElement);
+    const markupList = createListMarkup(data);
+    listElement.innerHTML = markupList;
+  }
+};
+
+const createListMarkup = data => {
+  return data
+    .map(
+      ({ name, flags }) =>
+        `<li><img src="${flags.png}" alt="${name.official}" width="60" height="40">${name.official}</li>`
+    )
+    .join('');
+};
+
+const createInfoMarkup = data => {
+  return data.map(
+    ({ name, capital, population, flags, languages }) =>
+      `<h1><img src="${flags.png}" alt="${
+        name.official
+      }" width="40" height="40">${name.official}</h1>
+      <p>Capital: ${capital}</p>
+      <p>Population: ${population}</p>
+      <p>Languages: ${Object.values(languages)}</p>`
+  );
+};
